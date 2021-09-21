22de426993fd473815e0fed88ce5010c1a1cd4b0
Alois Dirnaichner <alodi@directbox.com>
2021-09-21 15:10:51 +0200

    Update to include energy and non-energy costs per km for trucks.

--- inst/extdata/load_veh_costs.py
+++ inst/extdata/load_veh_costs.py
@@ -13,15 +13,18 @@ array = array.interp(
     year=np.array([2020, 2030, 2040, 2050]),
     kwargs={'fill_value': 'extrapolate'})
 
-cm = ct.TruckModel(array, cycle="Regional delivery" country="DE")
+cm = ct.TruckModel(array, cycle="Regional delivery", country="DE")
 
 cm.set_all()
 
-df = cm.array.sel(parameter="purchase cost").to_dataframe(name="purchase_cost").reset_index().drop(columns=["parameter", "value"])
+df_energy = cm.array.sel(parameter="energy cost").to_dataframe(name="energy_cost").reset_index().drop(columns=["parameter", "value"])
+df_total = cm.array.sel(parameter="total cost per km").to_dataframe(name="total_cost").reset_index().drop(columns=["parameter", "value"])
 
-df["unit"] = "€/veh."
+df = df_total.merge(df_energy)
+df["non-energy cost"] = df["total_cost"] - df["energy_cost"]
+df["unit"] = "€/km"
 
-df.to_csv("truck_costs.csv")
+df.to_csv("truck_costs_perkm.csv")
 
 
 cip = cc.CarInputParameters()
