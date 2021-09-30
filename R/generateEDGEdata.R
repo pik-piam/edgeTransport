7b0ecbdb57d6482bcf5217b0d73d85d018da9d78
Alois Dirnaichner <alodi@directbox.com>
2021-09-30 17:21:30 +0200

    Remove EDGEscenario_description file.

--- R/generateEDGEdata.R
+++ R/generateEDGEdata.R
@@ -67,7 +67,7 @@ generateEDGEdata <- function(input_folder, output_folder,
              2130, 2150)
   ## load mappings
   REMIND2ISO_MAPPING = fread(system.file("extdata", "regionmapping_21_EU11.csv", package = "edgeTransport"))[, .(iso = CountryCode,region = RegionCode)]
-  EDGEscenarios = fread(system.file("extdata", "EDGEscenario_description.csv", package = "edgeTransport"))
+
   GCAM2ISO_MAPPING = fread(system.file("extdata", "iso_GCAM.csv", package = "edgeTransport"))
   EDGE2teESmap = fread(system.file("extdata", "mapping_EDGE_REMIND_transport_categories.csv", package = "edgeTransport"))
   EDGE2CESmap = fread(system.file("extdata", "mapping_CESnodes_EDGE.csv", package = "edgeTransport"))
