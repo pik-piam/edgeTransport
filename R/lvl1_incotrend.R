59b3cba5d98dee3bfc09357729860455d7c887e1
Alois Dirnaichner <alodi@directbox.com>
2022-02-21 11:32:10 +0100

    Set HSR sw to 1.

--- R/lvl1_incotrend.R
+++ R/lvl1_incotrend.R
@@ -46,6 +46,8 @@ lvl1_preftrend <- function(SWS, preftab, calibdem, incocost, years,
   FVtarget[FVdt, sw := i.sw, on=c("region", "year", "vehicle_type", "technology")]
   FVtarget[year <= 2010 & is.na(sw), sw := 0]
 
+  FVtarget[subsector_L3 == "HSR", sw := 1]
+
   tmps <- filldt(FVdt[grepl("_tmp_", technology)], 2010)[
     , `:=`(sw=1, level="FV", techscen=unique(FVtarget$techscen), approx="linear")]
   tmps[, c("logit.exponent", "tot_price") := NULL]
