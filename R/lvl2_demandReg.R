453ffd799d37cbe2cc485f5db22633de733c43f0
Alois Dirnaichner <alodi@directbox.com>
2022-04-26 10:19:13 +0200

    render sspsdp demand regression factors a required argument

--- R/lvl2_demandReg.R
+++ R/lvl2_demandReg.R
@@ -17,7 +17,7 @@
 
 
 lvl2_demandReg <- function(tech_output, price_baseline, GDP_POP, smartlifestyle,
-                           SSP_scen, ssp_factors=NULL, regional_factors=NULL){
+                           SSP_scen, ssp_factors, regional_factors=NULL){
   rich <- var <- eps <- GDP_cap <- region <- eps1 <- eps2 <- GDP_val <- POP_val <- NULL
   index_GDP <- income_elasticity_freight_sm <- income_elasticity_freight_lo <- index_GDPcap <- NULL
   income_elasticity_pass_sm <- income_elasticity_pass_lo <- price_elasticity_pass_lo <- sector <- NULL
