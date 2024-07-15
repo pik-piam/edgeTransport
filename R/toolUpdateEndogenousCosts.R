68c4b331089ed358a3fdb12a41d0093e529c4a0d
johannah-pik <89136160+johannah-pik@users.noreply.github.com>
2024-07-15 11:47:05 +0200

    bugfix apply ICE ban only to EUR

--- R/toolUpdateEndogenousCosts.R
+++ R/toolUpdateEndogenousCosts.R
@@ -93,11 +93,13 @@ toolUpdateEndogenousCosts <- function(dataEndoCosts,
 
   # Change policy mask for ICEs when ban is activated
   if (isICEban) {
+    browser()
     #Ban is applied to EU28 or EUR in case of REMIND running on 12 regions
     affectedRegions <- unique(helpers$regionmappingISOto21to12[regionCode12 == "EUR"]$regionCode21)
     affectedRegions <- c(affectedRegions, "EUR")
     #affectedRegions <- affectedRegions[!affectedRegions == "UKI"]
-    policyMask[technology %in% c("Liquids", "Gases", "Hybrid electric"), policyMask := applyICEban(period, policyMask), by = c("period")]
+    policyMask[technology %in% c("Liquids", "Gases", "Hybrid electric") & region %in% affectedRegions,
+               policyMask := applyICEban(period, policyMask), by = c("period")]
   }
 
   #check whether policy mask is calculated correctly for respective technologys
