5df85d681b8697362c93d0a84f5c1b3b8a97e361
johannah-pik <89136160+johannah-pik@users.noreply.github.com>
2024-10-21 00:06:54 +0200

    include endogenous costs

--- R/toolEdgeTransportSA.R
+++ R/toolEdgeTransportSA.R
@@ -142,6 +142,7 @@ toolEdgeTransportSA <- function(SSPscen,
   if (isAnalyticsReported) {
     endogenousCostsIterations <- list()
     fleetVehNumbersIterations <- list()
+    costsDiscreteChoiceIterations <- list()
   }
 
   for (i in seq(1, iterations, 1)) {
@@ -177,8 +178,12 @@ toolEdgeTransportSA <- function(SSPscen,
                                                 genModelPar,
                                                 endogenousCosts$updatedEndogenousCosts,
                                                 helpers)
+    if (isAnalyticsReported) {
+      costsDiscreteChoiceIterations[[i]] <- lapply(copy(vehSalesAndModeShares$costsDiscreteChoice),
+                                               function(x){ x[, variable := paste0(variable, "|Iteration ", i)]})
+    }
     ESdemandFVsalesLevel <- toolCalculateFVdemand(sectorESdemand,
-                                                  vehSalesAndModeShares,
+                                                  vehSalesAndModeShares$shares,
                                                   helpers,
                                                   inputData$histESdemand,
                                                   baseYear)
@@ -189,7 +194,7 @@ toolEdgeTransportSA <- function(SSPscen,
     # Calculate vehicle stock for cars, trucks and busses -------
     fleetSizeAndComposition <- toolCalculateFleetComposition(ESdemandFVsalesLevel,
                                                              vehicleDepreciationFactors,
-                                                             vehSalesAndModeShares,
+                                                             vehSalesAndModeShares$shares,
                                                              inputData$annualMileage,
                                                              inputData$scenSpecLoadFactor,
                                                              helpers)
@@ -229,7 +234,7 @@ toolEdgeTransportSA <- function(SSPscen,
     histPrefs = histPrefs,
     fleetSizeAndComposition = fleetSizeAndComposition,
     endogenousCosts = endogenousCosts,
-    vehSalesAndModeShares = vehSalesAndModeShares,
+    vehSalesAndModeShares = vehSalesAndModeShares$shares,
     sectorESdemand = sectorESdemand,
     ESdemandFVsalesLevel = ESdemandFVsalesLevel,
     helpers = helpers
@@ -240,7 +245,8 @@ toolEdgeTransportSA <- function(SSPscen,
   outputRaw <- append(outputRaw, add)
 
   if (isAnalyticsReported) outputRaw <- append(outputRaw, list(endogenousCostsIterations = endogenousCostsIterations,
-                                    fleetVehNumbersIterations = fleetVehNumbersIterations))
+                                                               costsDiscreteChoiceIterations = costsDiscreteChoiceIterations,
+                                                               fleetVehNumbersIterations = fleetVehNumbersIterations))
 
   if (isStored) storeData(outputFolder = outputFolder, varsList = outputRaw)
 
