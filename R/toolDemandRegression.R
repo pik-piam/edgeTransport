620b98aad39803ea4910ebec23f19f30103016da
jmuessel <jarusch.muessel@pik-potsdam.de>
2025-03-10 16:44:51 +0100

    buildLib

--- R/toolDemandRegression.R
+++ R/toolDemandRegression.R
@@ -19,6 +19,7 @@ toolDemandRegression <- function(historicalESdemand, GDPperCapitaPPP, POP, genPa
                                   scenParDemRegression, scenParRegionalDemRegression, scenParDemandFactors,
                                     baseYear, policyStartYear, helpers) {
 
+  browser()
   # bind variables locally to prevent NSE notes in R CMD CHECK
   period <- sector <- value <- GDPpcPPP <- regionGDPpcPPP <- regionGDPppp <- regionalSummand <- NULL
   population <- GDPgrowthRate <- incomeElasticity <- GDPpcgrowthRate <- unit <- GDPpcterm <- NULL
